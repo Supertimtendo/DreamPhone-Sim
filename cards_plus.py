729be41e7f4fdf7cdf6a8da4a19c11ce44172708
Old Kid <patrickclaytonworking@gmail.com>
2023-01-23 22:42:43 -0700

    working on ending turn stuff "end_turn"

--- cards_plus.py
+++ cards_plus.py
@@ -1,5 +1,10 @@
+import itertools
 import random
 import copy
+import time
+from itertools import cycle
+
+
 
 class Player:  #class Player constructor
     def __init__(self, playernumber, cardsinhand,current_turn,playername):
@@ -24,6 +29,9 @@ c4 = Cards("Mike","555-5557")
 
 player1 = Player(1,[],False,"") #sets player1 with no cards in hand
 player2 = Player(2,[],False,"") #sets the variable player2
+
+player_list=[player1,player2]
+player_cycle = itertools.cycle(player_list)
 
 ##global stuff##
 
@@ -73,10 +81,12 @@ def starting_player():
                 if int(choice) == 1:
                     player1.current_turn = True
                     print("Player 1 will go first.")
+                    time.sleep(1)
                     break
                 if int(choice) == 2:
                     player2.current_turn = True
                     print("Player 2 will go first.")
+                    time.sleep(1)
                     break
                 else: print("Not a valid number.")
 
@@ -92,23 +102,40 @@ def starting_player():
                 else: print("Not a valid word.")
         else: print("Not a valid choice.")
 
+def next_player():
+    return next(player_cycle)
+
+def end_turn():
+    print(player_cycle)
+    print("Current player",whos_turn().playername)
+    if player_cycle == whos_turn():
+        print("test if the same")
+#        print(player_cycle.playername,"just finished their turn.",player_cycle.current_turn)
+#        whos_turn().current_turn = False
+#    if player_cycle != whos_turn():
+#        next_player().current_turn = True
+#        print(whos_turn().playername,"Is up!")
+#        print(whos_turn().playername, whos_turn().current_turn)
+
+#
+#    if whos_turn().current_turn == True:
+#        whos_turn().current_turn = False
+
+
 def shuffle():  # shuffles the game deck
     random.shuffle(game_deck)
     print("Cards in the game deck have been shuffled. \n")
 
 def draw(num_count):  # function to move cards from the game deck into your hand
-    p = whos_turn()
     if len(game_deck) == 0: #checks for an empty draw deck and displays an error if you draw from it
         print("Cannot draw any cards, the deck is empty.\n")
     else:
         if int(len(game_deck)) < num_count: #checks that you aren't trying to draw more cards than exist in the deck
             print("You can't draw more cards than the deck contains.\n")
         else:
-            print(p.playername, "drew", num_count, "cards.")
+            print(whos_turn().playername, "drew", num_count, "cards.")
             for i in range(num_count):  # I counts the number of draw_count specified
-                if p.playernumber == 1: player1.cardsinhand.append(game_deck.pop(0))
-                if p.playernumber == 2: player2.cardsinhand.append(game_deck.pop(0))
- #               in_hand.append(game_deck.pop(0))  # moves top card from game_deck list and adds it to in_hand
+                whos_turn().cardsinhand.append(game_deck.pop(0))
     show_hand()
 
 def ask_for_num():
@@ -131,22 +158,23 @@ def ask_for_num():
             print("Incorrect entry. Please enter a number, or type 'back' or '0' to leave.")
 
 def discard(num_count):
-    if len(in_hand) == 0:  # check that your hand is not empty
-        print("you have no cards to discard.\n")
+    if len(whos_turn().cardsinhand) == 0:  # check that your hand is not empty
+        print("you have no cards to discard.")
+        time.sleep(1)
     else:
-        if int(len(in_hand)) < num_count: #checks that you aren't trying to discard more cards than are in your hand
+        if int(len(whos_turn().cardsinhand)) < num_count: #checks that you aren't trying to discard more cards than are in your hand
             print("You cannot discard more cards than you have in hand.")
         else:
             print("You are discarding", num_count, "cards.")
             for i in range(num_count):  # i counts the number of discard_count specified
-                discard_pile.append(in_hand.pop(0)) #moves in hand card to discard pile
-        count_discard()
+                discard_pile.append(whos_turn().cardsinhand.pop(i)) #moves in hand card to discard pile
 
 def discard_choice():
     loop = 1
     while loop == 1:
-        if len(in_hand) == 0:  # check that your hand is not empty
+        if len(whos_turn().cardsinhand) == 0:  # check that your hand is not empty
             print("you have no cards to discard.")
+            time.sleep(1)
             break
         else:
             show_hand()
@@ -156,11 +184,12 @@ def discard_choice():
                 print("Exiting discard choice...")
                 break #leaves discard choice loop
             else:
-                for i in range(len(in_hand)):  #runs through the list count of in_hand
-                    if card_choice == (in_hand[i].name).lower(): #checks if choice is in your hand
-                        print(in_hand[i].name,"from your hand has been discarded.")
-                        discard_pile.append(in_hand.pop(i)) #adds card to discard pile based on in_hand index num
+                for i in range(len(whos_turn().cardsinhand)):  #runs through the list count of in_hand
+                    if card_choice == (whos_turn().cardsinhand[i].name.lower()): #checks if choice is in your hand
+                        print(whos_turn().cardsinhand[i].name,"from your hand has been discarded.")
+                        discard_pile.append(whos_turn().cardsinhand.pop(i)) #adds card to discard pile based on in_hand index num
                         loop = 0
+                        time.sleep(1)
                         break
 
                 else: #if not 0 cards and if not a in_hand[i].name, incorrect choice
@@ -180,8 +209,10 @@ def reshuffle():
 def count_discard():
     if len(discard_pile) == 0:
         print ("There are no cards in the discard pile.\n")
+        time.sleep(1)
     else:
         print("number of cards in the discard pile:", len(discard_pile))
+        time.sleep(1)
 
 def show_discard():
     if len(discard_pile) == 0:
@@ -191,34 +222,19 @@ def show_discard():
         for i in discard_pile: print(i.name)
 
 def count_hand():
-    p = whos_turn()
-    if p == 1:
-        if len(player1.cardsinhand) == 0:
-            print ("There are no cards in your hand.\n")
-        else:
-            print("number of cards in your hand are:", len(player1.cardsinhand))
-    if p == 2:
-        if len(player2.cardsinhand) == 0:
-            print ("There are no cards in your hand.\n")
-        else:
-            print("number of cards in your hand are:", len(player2.cardsinhand))
+    if len(whos_turn().cardsinhand) == 0:
+        print ("There are no cards in your hand.\n")
+    else:
+        print("number of cards in your hand are:", len(whos_turn().cardsinhand))
 
 def show_hand():
-    p = whos_turn()
-    if p.playernumber == 1:
-        if len(player1.cardsinhand) == 0:
-            print("There are no cards in your hand.\n")
-        else:
-            print("Cards in", p.playername, "'s hand are:")
-            for i in player1.cardsinhand:
-                print (i.name)
-    if p.playernumber == 2:
-        if len(in_hand) == 0:
-            print("There are no cards in your hand.\n")
-        else:
-            print("Cards in", p.playername, "'s hand are:")
-            for i in player2.cardsinhand:
-                print (i.name)
+    if len(whos_turn().cardsinhand) == 0:
+        print("There are no cards in your hand.\n")
+    else:
+        print("Cards in", whos_turn().playername, "'s hand are:")
+        for i in whos_turn().cardsinhand:
+            print (i.name)
+            time.sleep(.5)
 
 def count_deck():
     if len(game_deck) == 0:
@@ -234,21 +250,22 @@ def show_deck():
         for i in game_deck: print(i.name)
 
 def look():
-    if len(in_hand) == 0:
+    if len(whos_turn().cardsinhand) == 0:
         print("There are no cards in your hand.\n")
     else:
-        print("You look closely at the cards in your hand.")
-        for i in in_hand:
+        print(whos_turn().playername,"looks closely at the cards in their hand.")
+        for i in whos_turn().cardsinhand:
             print(str(i.name),"- Phone#:",(i.detail))
-
+            time.sleep(.5)
+    time.sleep(2)
 def count():
     #print("====Status====\nDraw Deck:",len(game_deck),"\nHand:",len(in_hand),"\nDiscard:",len(discard_pile),"\n")
-    print(f"====Status====\nDraw Deck: {len(game_deck)}\nHand: {len(in_hand)}\nDiscard: {len(discard_pile)}\n")
-
+    print(f"====Status====\nDraw Deck: {len(game_deck)}\n{whos_turn().playername}'s Hand: {len(whos_turn().cardsinhand)}\nDiscard: {len(discard_pile)}\n")
+    time.sleep(1.5)
 # now on to the main loop. it simply checks for inputs to run the outlined functions. nothing too crazy
 
 def game_loop():
-    valid_choices=["look","shuffle","draw","discard","discard choice","reshuffle","count","show","count deck","count hand","count discard","show deck","show hand","show discard","more"]
+    valid_choices=["end","look","shuffle","draw","discard","discard choice","reshuffle","count","show","count deck","count hand","count discard","show deck","show hand","show discard","more"]
     print("\nWelcome to deck game. This is a test routine for cards moving around in Python lists.\n")
     name_players()
     starting_player()
@@ -301,6 +318,7 @@ def game_loop():
             if choice == 'show hand': show_hand()
             if choice == 'show discard': show_discard()
             if choice == 'look': look()
+            if choice == 'end': end_turn()
 
             if choice == 'more':
                 print("You can type 'show' or 'count' to show or count your hand, draw pile and discard pile. Using 'show' or 'count' plus 'hand', 'deck' or 'discard' will display or count the individual respective stacks.")
